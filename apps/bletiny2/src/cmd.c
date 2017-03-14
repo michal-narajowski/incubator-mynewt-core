31062d027e3db032e72e7a9fbe2718e50f72c761
Michał Narajowski <michal.narajowski@codecoup.pl>
2017-03-14 16:25:04 +0100

    apps/bletiny2: add connect command

--- apps/bletiny2/src/cmd.c
+++ apps/bletiny2/src/cmd.c
@@ -18,22 +18,198 @@
  */
 
 #include <inttypes.h>
+#include <errno.h>
+#include <string.h>
+#include "bsp/bsp.h"
+
+#include "nimble/ble.h"
+#include "nimble/nimble_opt.h"
+#include "nimble/hci_common.h"
+#include "host/ble_gap.h"
+#include "host/ble_hs_adv.h"
+#include "host/ble_sm.h"
+#include "host/ble_eddystone.h"
+#include "host/ble_hs_id.h"
+#include "services/gatt/ble_svc_gatt.h"
 
 #include "newshell/console/console.h"
 #include "newshell/shell/shell.h"
 
 #include "cmd.h"
+#include "bletiny.h"
 
 #define BTSHELL_MODULE "btshell"
 
-static const struct shell_cmd commands[] = {
-    { NULL, NULL, NULL }
+
+static struct kv_pair cmd_own_addr_types[] = {
+    { "public",     BLE_OWN_ADDR_PUBLIC },
+    { "random",     BLE_OWN_ADDR_RANDOM },
+    { "rpa_pub",    BLE_OWN_ADDR_RPA_PUBLIC_DEFAULT },
+    { "rpa_rnd",    BLE_OWN_ADDR_RPA_RANDOM_DEFAULT },
+    { NULL }
 };
+
+static struct kv_pair cmd_peer_addr_types[] = {
+    { "public",     BLE_ADDR_PUBLIC },
+    { "random",     BLE_ADDR_RANDOM },
+    { "public_id",  BLE_ADDR_PUBLIC_ID },
+    { "random_id",  BLE_ADDR_RANDOM_ID },
+    { NULL }
+};
+
+/*****************************************************************************
+ * $connect                                                                  *
+ *****************************************************************************/
+
+static int
+cmd_connect(int argc, char **argv)
+{
+    struct ble_gap_conn_params params;
+    int32_t duration_ms;
+    ble_addr_t peer_addr;
+    ble_addr_t *peer_addr_param = &peer_addr;
+    int own_addr_type;
+    int rc;
+
+    if (argc > 1 && strcmp(argv[1], "cancel") == 0) {
+        rc = bletiny_conn_cancel();
+        if (rc != 0) {
+            console_printf("connection cancel fail: %d\n", rc);
+            return rc;
+        }
+
+        return 0;
+    }
+
+    peer_addr.type = parse_arg_kv_default("peer_addr_type", cmd_peer_addr_types,
+                                          BLE_ADDR_PUBLIC, &rc);
+    if (rc != 0) {
+        console_printf("invalid 'peer_addr_type' parameter\n");
+        return rc;
+    }
+
+    rc = parse_arg_mac("peer_addr", peer_addr.val);
+    if (rc == ENOENT) {
+        /* Allow "addr" for backwards compatibility. */
+        rc = parse_arg_mac("addr", peer_addr.val);
+    }
+
+    if (rc == ENOENT) {
+        /* With no "peer_addr" specified we'll use white list */
+        peer_addr_param = NULL;
+    } else if (rc != 0) {
+        console_printf("invalid 'peer_addr' parameter\n");
+        return rc;
+    }
+
+    own_addr_type = parse_arg_kv_default("own_addr_type", cmd_own_addr_types,
+                                         BLE_OWN_ADDR_PUBLIC, &rc);
+    if (rc != 0) {
+        console_printf("invalid 'own_addr_type' parameter\n");
+        return rc;
+    }
+
+    params.scan_itvl = parse_arg_uint16_dflt("scan_itvl", 0x0010, &rc);
+    if (rc != 0) {
+        console_printf("invalid 'scan_itvl' parameter\n");
+        return rc;
+    }
+
+    params.scan_window = parse_arg_uint16_dflt("scan_window", 0x0010, &rc);
+    if (rc != 0) {
+        console_printf("invalid 'scan_window' parameter\n");
+        return rc;
+    }
+
+    params.itvl_min = parse_arg_uint16_dflt(
+        "itvl_min", BLE_GAP_INITIAL_CONN_ITVL_MIN, &rc);
+    if (rc != 0) {
+        console_printf("invalid 'itvl_min' parameter\n");
+        return rc;
+    }
+
+    params.itvl_max = parse_arg_uint16_dflt(
+        "itvl_max", BLE_GAP_INITIAL_CONN_ITVL_MAX, &rc);
+    if (rc != 0) {
+        console_printf("invalid 'itvl_max' parameter\n");
+        return rc;
+    }
+
+    params.latency = parse_arg_uint16_dflt("latency", 0, &rc);
+    if (rc != 0) {
+        console_printf("invalid 'latency' parameter\n");
+        return rc;
+    }
+
+    params.supervision_timeout = parse_arg_uint16_dflt("timeout", 0x0100, &rc);
+    if (rc != 0) {
+        console_printf("invalid 'timeout' parameter\n");
+        return rc;
+    }
+
+    params.min_ce_len = parse_arg_uint16_dflt("min_ce_len", 0x0010, &rc);
+    if (rc != 0) {
+        console_printf("invalid 'min_ce_len' parameter\n");
+        return rc;
+    }
+
+    params.max_ce_len = parse_arg_uint16_dflt("max_ce_len", 0x0300, &rc);
+    if (rc != 0) {
+        console_printf("invalid 'max_ce_len' parameter\n");
+        return rc;
+    }
+
+    duration_ms = parse_arg_long_bounds_default("dur", 1, INT32_MAX, 0, &rc);
+    if (rc != 0) {
+        console_printf("invalid 'dur' parameter\n");
+        return rc;
+    }
+
+    rc = bletiny_conn_initiate(own_addr_type, peer_addr_param, duration_ms,
+                               &params);
+    if (rc != 0) {
+        return rc;
+    }
+
+    return 0;
+}
+
+static const struct shell_param connect_params[] = {
+    {"cancel", "cancel connection procedure"},
+    {"peer_addr_type", ""},
+    {"peer_addr", ""},
+    {"own_addr_type", ""},
+    {"scan_interval", ""},
+    {"scan_window", ""},
+    {"interval_min", ""},
+    {"interval_max", ""},
+    {"latency", ""},
+    {"timeout", ""},
+    {"min_conn_event_len", ""},
+    {"max_conn_event_len", ""},
+    {"duration", ""},
+    {NULL, NULL}
+};
+
+static const struct shell_cmd_help connect_help = {
+    .summary = "connect",
+    .usage = "connect usage",
+    .params = connect_params,
+};
+
+static const struct shell_cmd btshell_commands[] = {
+    {
+        .cmd_name = "connect",
+        .cb = cmd_connect,
+        .help = &connect_help,
+    },
+    { NULL, NULL, NULL },
+};
+
 
 void
 cmd_init(void)
 {
-    shell_register(BTSHELL_MODULE, commands);
+    shell_register(BTSHELL_MODULE, btshell_commands);
     shell_register_default_module(BTSHELL_MODULE);
-    console_printf("HELLO!\n");
 }
